2fa15b1d760cd200f369a941504412726602be34
Chefsspecial <93719101+Chefsspecial@users.noreply.github.com>
2023-02-28 10:10:59 +0100

    Tools for data Science file
    Here is the exam file for the coursera course tools for data science

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1> My Jupyter Notebook on IBM Watson Studion</H1>
+
+# <B> Manon ten Have </B>
+# <br> Data Analyst
+# 
+
+# <i> I am interested in data science because I find working with data exciting, enjoy solving complex problems, and am intellectually curious about the field. As someone who is drawn to puzzles and enjoys digging into data to uncover insights, data science feels like a natural fit for me. I am eager to learn more about the mathematical and statistical techniques used in data science, as well as the programming languages and tools commonly used by data scientists. Ultimately, I am excited about the potential to use data science to make meaningful contributions to a wide range of fields, from healthcare to business to social justice. </i>
+# 
+# 
+# 
+
+# In[6]:
+
+
+# Prices of fruit
+oranges = 2
+apple = 0.5
+grapes = 3.5
+banana = 1
+
+# How can I calculate my total shopping expense?
+expense = oranges + apple + grapes + banana
+
+# Print the result
+print(expense)
+
+
+# ~~Reasons not to buy fruit~~
+# 
+# Reasons to buy fruit:
+# 1. Fruits are delicious
+# 2. Fruits are healthy
+# 3. Fruits make your brain happy
+# 4. Fruits also have sugar
+# 
+# Here is a link to a picture of my favorite food:
+# "[strawberries](
+# https://www.bing.com/images/search?view=detailV2&ccid=oSY8Dw8f&id=6AFB0661468A0E0316141B194403B3D647A29ED9&thid=OIP.oSY8Dw8fVAdN_9qj5njoqQHaE9&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.a1263c0f0f1f54074dffdaa3e678e8a9%3frik%3d2Z6iR9azA0QZGw%26riu%3dhttp%253a%252f%252fwww.wur.nl%252fupload%252fc015d82c-fb57-4828-b739-87bdae40d299_aardbei.jpg%26ehk%3dPXFUydSoXx9YMqbFjVIkm7Wip9%252fH6AVVpogUTUGxvhE%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=669&expw=1000&q=aardbei&simid=608055009105686914&FORM=IRPRST&ck=F8EC671FECD0C1CCE6A9C97B322ABADA&selectedIndex=12&ajaxhist=0&ajaxserp=0)"
